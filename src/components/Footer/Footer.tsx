ba6454c8ef4da688def5c2a258c98d97dc8869e8
Isaac-MCastanho <fgcastanho@hotmail.com>
2023-04-27 18:37:14 -0300

    Implementando token no Nav e footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,88 +4,99 @@ import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import { Typography, Grid } from "@material-ui/core";
 import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+import { TokenState } from "../../store/tokens/tokensReducer";
 
 export function Footer() {
-	return (
-		<>
-			<Grid
-				container
-				direction="row"
-				justifyContent="center"
-				alignItems="center"
-			>
-				<Box
-					display={"flex"}
-					alignItems="center"
-					bgcolor="primary.dark"
-					style={{ height: "120px" }}
-					width={"100%"}
-					justifyContent={"space-around"}
-				>
-					<Box>
-						<Box
-							paddingTop={1}
-							display="flex"
-							alignItems="center"
-							justifyContent="center"
-						>
-							<Typography
-								variant="h5"
-								align="center"
-								gutterBottom
-								style={{ color: "white" }}
-							>
-								Siga-nos nas redes sociais{" "}
-							</Typography>
-						</Box>
-						<Box display="flex" alignItems="center" justifyContent="center">
-							<a
-								href="https://www.facebook.com/generationbrasil"
-								target="_blank"
-							>
-								<FacebookIcon style={{ fontSize: 60, color: "white" }} />
-							</a>
-							<a
-								href="https://www.instagram.com/generationbrasil/"
-								target="_blank"
-							>
-								<InstagramIcon style={{ fontSize: 60, color: "white" }} />
-							</a>
-							<a
-								href="https://www.linkedin.com/school/generationbrasil/"
-								target="_blank"
-							>
-								<LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-							</a>
-						</Box>
-					</Box>
-					<Box>
-						<Box paddingTop={1}>
-							<Typography
-								variant="h5"
-								align="center"
-								gutterBottom
-								style={{ color: "white" }}
-								component={"span"}
-							>
-								© 2023 Copyright:{" "}
-							</Typography>
+	const token = useSelector<TokenState, TokenState["token"]>(
+		(state) => state.token
+	);
 
-							<a target="_blank" href="https://brasil.generation.org">
+	let footerComponente;
+
+	if (token !== "") {
+		footerComponente = (
+			<>
+				<Grid
+					container
+					direction="row"
+					justifyContent="center"
+					alignItems="center"
+				>
+					<Box
+						display={"flex"}
+						alignItems="center"
+						bgcolor="primary.dark"
+						style={{ height: "120px" }}
+						width={"100%"}
+						justifyContent={"space-around"}
+					>
+						<Box>
+							<Box
+								paddingTop={1}
+								display="flex"
+								alignItems="center"
+								justifyContent="center"
+							>
 								<Typography
 									variant="h5"
+									align="center"
 									gutterBottom
 									style={{ color: "white" }}
+								>
+									Siga-nos nas redes sociais{" "}
+								</Typography>
+							</Box>
+							<Box display="flex" alignItems="center" justifyContent="center">
+								<a
+									href="https://www.facebook.com/generationbrasil"
+									target="_blank"
+								>
+									<FacebookIcon style={{ fontSize: 60, color: "white" }} />
+								</a>
+								<a
+									href="https://www.instagram.com/generationbrasil/"
+									target="_blank"
+								>
+									<InstagramIcon style={{ fontSize: 60, color: "white" }} />
+								</a>
+								<a
+									href="https://www.linkedin.com/school/generationbrasil/"
+									target="_blank"
+								>
+									<LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+								</a>
+							</Box>
+						</Box>
+						<Box>
+							<Box paddingTop={1}>
+								<Typography
+									variant="h5"
 									align="center"
+									gutterBottom
+									style={{ color: "white" }}
 									component={"span"}
 								>
-									brasil.generation.org
+									© 2023 Copyright:{" "}
 								</Typography>
-							</a>
+
+								<a target="_blank" href="https://brasil.generation.org">
+									<Typography
+										variant="h5"
+										gutterBottom
+										style={{ color: "white" }}
+										align="center"
+										component={"span"}
+									>
+										brasil.generation.org
+									</Typography>
+								</a>
+							</Box>
 						</Box>
 					</Box>
-				</Box>
-			</Grid>
-		</>
-	);
+				</Grid>
+			</>
+		);
+	}
+	return <>{footerComponente}</>;
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./Navbar.css";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ThemeActions, useTheme } from "../../contexts/theme/ThemeContext";
 import Logo from "../../assets/img/The_Green001.png";
 
@@ -10,97 +10,132 @@ import Switch from "@material-ui/core/Switch";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import useLocalStorage from "react-use-localstorage";
+import { useDispatch, useSelector } from "react-redux";
+import { TokenState } from "../../store/tokens/tokensReducer";
+import { addToken } from "../../store/tokens/action";
+import { toast } from "react-toastify";
 
 export function Navbar() {
-	const [token, setToken] = useLocalStorage("token");
+	const token = useSelector<TokenState, TokenState["token"]>(
+		(state) => state.token
+	);
+	const history = useNavigate();
+	const dispatch = useDispatch();
 	const [checked, setChecked] = useState(false);
-	const { state: themeContext, dispatch } = useTheme();
+	const { state: themeContext, dispatch: themeDispatch } = useTheme();
 
 	const logout = () => {
-		setToken("");
+		dispatch(addToken(""));
+		themeDispatch(ThemeActions.lightMode);
+		toast.info("usuario deslogado com sucesso", {
+			position: "top-right",
+			autoClose: 3000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+
+		history("/login");
 	};
 
 	const toggleChecked = () => {
 		setChecked((prev) => !prev);
-		dispatch(
+		themeDispatch(
 			themeContext.dark ? ThemeActions.lightMode : ThemeActions.darkMode
 		);
 	};
-	return (
-		<>
-			<AppBar
-				id="navbar"
-				position="fixed"
-				style={{ background: themeContext.bg.primary }}
-			>
-				<Toolbar
-					variant="regular"
-					style={{ color: themeContext.text.primary.dark }}
+
+	let navbarComponent;
+
+	if (token !== "") {
+		navbarComponent = (
+			<>
+				<AppBar
+					id="navbar"
+					position="fixed"
+					style={{ background: themeContext.bg.primary }}
 				>
-					<Box display={"flex"} justifyContent={"space-between"} width={"100%"}>
-						<Box style={{ cursor: "pointer" }}>
-							<Typography variant="h5" color="inherit">
-								<img
-									src={Logo}
-									alt=""
-									className="logo"
-									style={{ maxWidth: "100px" }}
-								/>
-							</Typography>
-						</Box>
+					<Toolbar
+						variant="regular"
+						style={{ color: themeContext.text.primary.dark }}
+					>
+						<Box
+							display={"flex"}
+							justifyContent={"space-between"}
+							width={"100%"}
+						>
+							<Box style={{ cursor: "pointer" }}>
+								<Typography variant="h5" color="inherit">
+									<img
+										src={Logo}
+										alt=""
+										className="logo"
+										style={{ maxWidth: "100px" }}
+									/>
+								</Typography>
+							</Box>
 
-						<Box display="flex" alignItems="center" justifyContent="center">
-							<Link to="/home">
-								<Box
-									display="inherit"
-									alignItems="center"
-									mx={1}
-									style={{ cursor: "pointer" }}
-								>
-									<Typography variant="h6" color="inherit">
-										Home
-									</Typography>
-								</Box>
-							</Link>
-
-							<Link to="/temas">
-								<Box
-									display="inherit"
-									alignItems="center"
-									mx={1}
-									style={{ cursor: "pointer" }}
-								>
-									<Typography variant="h6" color="inherit">
-										Temas
-									</Typography>
-								</Box>
-							</Link>
-
-							<Link to="/cadastrartema">
-								<Box
-									display="inherit"
-									alignItems="center"
-									mx={1}
-									style={{ cursor: "pointer" }}
-								>
-									<Typography variant="h6" color="inherit">
-										Cadastrar Temas
-									</Typography>
-								</Box>
-							</Link>
-
-							<Box display="inherit" alignItems="center" mx={1}>
-								<Link onClick={logout} to="/login">
-									<Typography variant="h6" color="inherit">
-										Logout
-									</Typography>
+							<Box display="flex" alignItems="center" justifyContent="center">
+								<Link to="/home">
+									<Box
+										display="inherit"
+										alignItems="center"
+										mx={1}
+										style={{ cursor: "pointer" }}
+									>
+										<Typography variant="h6" color="inherit">
+											Home
+										</Typography>
+									</Box>
 								</Link>
+
+								<Link to="/temas">
+									<Box
+										display="inherit"
+										alignItems="center"
+										mx={1}
+										style={{ cursor: "pointer" }}
+									>
+										<Typography variant="h6" color="inherit">
+											Temas
+										</Typography>
+									</Box>
+								</Link>
+
+								<Link to="/cadastrartema">
+									<Box
+										display="inherit"
+										alignItems="center"
+										mx={1}
+										style={{ cursor: "pointer" }}
+									>
+										<Typography variant="h6" color="inherit">
+											Cadastrar Temas
+										</Typography>
+									</Box>
+								</Link>
+
+								<Box display="inherit" alignItems="center" mx={1}>
+									<Link onClick={logout} to="/login">
+										<Typography variant="h6" color="inherit">
+											Logout
+										</Typography>
+									</Link>
+								</Box>
 							</Box>
 						</Box>
-					</Box>
-					<Switch color="primary" checked={checked} onChange={toggleChecked} />
-				</Toolbar>
-			</AppBar>
-		</>
-	);
+						<Switch
+							color="primary"
+							checked={checked}
+							onChange={toggleChecked}
+						/>
+					</Toolbar>
+				</AppBar>
+			</>
+		);
+	}
+	return <>{navbarComponent}</>;
 }
